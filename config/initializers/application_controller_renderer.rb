2f5f61d01f0986145b61e999189bb6a18872a674
horkar667 <jmmpp413@yahoo.co.jp>
2021-08-04 11:47:10 +0000

    [Start]railsの基礎

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '26c54c0d097ae3975a653f1c812d8901cd2673dff0328c264e7d9734a59bac7739f4e2f55b42e7aaa06941b39f939f474e30d7eea478cd15b97328c808d93d4c'
